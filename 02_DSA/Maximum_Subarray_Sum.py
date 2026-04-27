97e7f39ee2d5588004fdb3bcc11cb306db2d54f5
md mohsin <siam.mohsin2005@gmail.com>
2026-04-27 21:50:40 +0600

    Implement Maximum Subarray Sum

---
+++ 02_DSA/Maximum_Subarray_Sum.py
@@ -0,0 +1,11 @@
+def max_subarray(nums):
+    max_current = max_global = nums[0]
+    
+    for i in range(1, len(nums)):
+        max_current = max(nums[i], max_current + nums[i])
+        max_global = max(max_global, max_current)
+        
+    return max_global
+
+
+print(max_subarray([1, 2, 5, 6, 2]))
