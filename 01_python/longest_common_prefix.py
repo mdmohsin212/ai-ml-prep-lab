dde04793ae11769f15e0f81afb6ad73c20e655d1
md mohsin <siam.mohsin2005@gmail.com>
2026-05-08 20:28:47 +0600

    Implement longest common prefix

---
+++ 01_python/longest_common_prefix.py
@@ -0,0 +1,14 @@
+def longest_common_prefix(strings):
+    if not strings:
+        return ""
+    
+    prefix = strings[0]
+    
+    for s in strings[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+    return prefix
+
+
+strings = ["flower", "flow", "flight"]
+print(longest_common_prefix(strings))
