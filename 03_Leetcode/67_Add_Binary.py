24762ca88709f0040a1b53bc95d813006a210b7b
md mohsin <siam.mohsin2005@gmail.com>
2026-05-13 21:47:04 +0600

    67. Add Binary

--- 03_Leetcode/67_Add_Binary.py
+++ 03_Leetcode/67_Add_Binary.py
@@ -1,4 +1,24 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = int(a, 2) + int(b, 2)
-        return bin(a)[2:]
+        res =  []
+        carry = 0
+
+        i = len(a) - 1
+        j = len(b) - 1
+
+        while i >= 0 or j >= 0 or carry:
+            total_sum = carry
+
+            if i >= 0:
+                total_sum += int(a[i])
+                i -= 1
+
+            if j >= 0:
+                total_sum += int(b[j])
+                j -= 1
+            
+            res.append(str(total_sum % 2))
+
+            carry = total_sum // 2
+
+        return "".join(res[::-1])
