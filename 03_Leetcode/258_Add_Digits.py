e4507c23af67ba2590980085d61e818bd492f873
md mohsin <siam.mohsin2005@gmail.com>
2026-05-20 19:38:31 +0600

    Slove leetcode 258. Add Digits

--- 03_Leetcode/258_Add_Digits.py
+++ 03_Leetcode/258_Add_Digits.py
@@ -1,13 +1,12 @@
-ans = 0
-num = 38
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            ans = 0
+            while num > 0:
+                tmp = num % 10
+                ans += tmp
+                num = num // 10
+            
+            num = ans
 
-while num > 9:
-    ans = 0
-    while num > 0:
-        tmp = num % 10
-        ans += tmp
-        num = num // 10
-    
-    num = ans
-
-print(num)
+        return num
