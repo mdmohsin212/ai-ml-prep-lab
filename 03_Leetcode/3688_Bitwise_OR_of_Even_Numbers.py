8c143124db5cc1af0c2d2c176b1a52594e0792b9
md mohsin <siam.mohsin2005@gmail.com>
2026-05-23 17:37:11 +0600

    Slove leetcode 3688. Bitwise OR of Even Numbers

---
+++ 03_Leetcode/3688_Bitwise_OR_of_Even_Numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def evenNumberBitwiseORs(self, nums: List[int]) -> int:
+        ans = 0
+        for num in nums:
+            if num % 2 == 0:
+                ans |= num
+
+        return ans
