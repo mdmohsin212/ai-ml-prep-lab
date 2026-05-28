0aeb3be7b32b4483fa4260c64148605b86d06c3b
md mohsin <siam.mohsin2005@gmail.com>
2026-05-28 20:05:54 +0600

    update

--- 03_Leetcode/13_Roman_to_Integer.py
+++ 03_Leetcode/13_Roman_to_Integer.py
@@ -1,10 +1,7 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         data = {
-            "I" : 1,
-            "V" : 5,
-            "X" : 10,
-            "L" : 50,
+            "I" : 1, "V" : 5,"X" : 10, "L" : 50,
             "C" : 100,
             "D" : 500,
             "M" : 1000
