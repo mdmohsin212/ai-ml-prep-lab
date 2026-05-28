c5c02245700494b82056e039a7e6ff29548d0ee6
md mohsin <siam.mohsin2005@gmail.com>
2026-05-28 20:06:09 +0600

    update

--- 03_Leetcode/13_Roman_to_Integer.py
+++ 03_Leetcode/13_Roman_to_Integer.py
@@ -2,9 +2,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         data = {
             "I" : 1, "V" : 5,"X" : 10, "L" : 50,
-            "C" : 100,
-            "D" : 500,
-            "M" : 1000
+            "C" : 100, "D" : 500, "M" : 1000
         }
         ans = 0
         n = len(s)
