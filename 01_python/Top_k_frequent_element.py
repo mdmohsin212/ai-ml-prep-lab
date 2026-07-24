f247a21aa3173acdcb9d57e3b222963e7bd8d9db
md mohsin <siam.mohsin2005@gmail.com>
2026-07-24 22:22:43 +0600

    Top k frequent element

--- 01_python/Top_k_frequent_element.py
+++ 01_python/Top_k_frequent_element.py
@@ -1,2 +1,9 @@
 from collections import Counter
 from heapq import nlargest
+
+def top_k_frequent(values, k):
+    if k <= 0:
+        return []
+    
+    counts = Counter(values)
+    return [item for item, _ in nlargest(k, counts.items(), key=lambda p: p[1])]
