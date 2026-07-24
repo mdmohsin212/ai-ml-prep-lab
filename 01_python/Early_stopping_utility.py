414e9b93e49a3550d3a915a4f8328d51e8259865
md mohsin <siam.mohsin2005@gmail.com>
2026-07-24 22:02:52 +0600

    Implement Early stopping utility

--- 01_python/Early_stopping_utility.py
+++ 01_python/Early_stopping_utility.py
@@ -1,2 +1,14 @@
 class EarlyStopping:
-    
+    def __init__(self, patience=3, min_delta=0.0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.best = float("-inf")
+        self.bad_epochs = 0
+        
+    def update(self, score):
+        if score > self.best + self.min_delta:
+            self.best = score
+            self.bad_epochs = 0
+        else:
+            self.bad_epochs += 1
+        return self.bad_epochs >= self.patience
