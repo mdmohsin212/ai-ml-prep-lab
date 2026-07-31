99c12c2eb1e60c5c70eb50801f3b97288dd2ebab
md mohsin <siam.mohsin2005@gmail.com>
2026-07-31 21:51:33 +0600

    update

--- 01_python/list_analyzer.py
+++ 01_python/list_analyzer.py
@@ -3,7 +3,6 @@ def list_analyze(items : list):
     sorted_unique = sorted(unique)
     
     freq_list = {item : items.count(item) for item in unique}
-    
     return unique, sorted_unique, freq_list
 
 a = [5, 4, 5, 2, 2, 1]
